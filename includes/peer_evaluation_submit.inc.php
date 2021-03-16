d9344a6f726dcba820ed9081c381de1304a3d672
Greg Fairbanks <gregory12006@yahoo.com>
2021-03-16 13:55:10 -0400

    Edited comments

--- includes/peer_evaluation_submit.inc.php
+++ includes/peer_evaluation_submit.inc.php
@@ -41,57 +41,11 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
 } else {
 	if(isset($_SESSION["evalStart"])){
 		$startDate = $_SESSION["evalStart"];
-		echo $startDate;
-		echo $date;
 	}
 	mysqli_stmt_bind_param($stmt, "iiss", $studentIndex, $loggedInStudent, $startDate, $date);
 	mysqli_stmt_execute($stmt);
+
+
+
+	header("Location: ../evaluationsuccess.php?result=success");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$sql = "INSERT INTO student_criterion_score (criterion_id, score, student_id, peerEval_id) VALUES ()";
-$stmt = mysqli_stmt_init($conn);
-
-if (!mysqli_stmt_prepare($stmt, $sql)) {
-	header("Location: ../index.php?error=sqlerror");
-	exit();
-} else {
-	mysqli_stmt_bind_param($stmt, "s", $email);
-	mysqli_stmt_execute($stmt);
-
-	//grabs the result for the stmt
-	$result = mysqli_stmt_get_result($stmt);
-
-	$i = 0;
-	while ($row = $result->fetch_assoc()) {
-		$_SESSION['student_id'][$i] = $row["student_id"];
-		$_SESSION['names'][$i] = $row["first_name"] . " " . $row["last_name"];
-		$i++;
-	}
-
-
-
-
-	//header("Location: ../evaluationsuccess.php?result=success");
-}
-*/
