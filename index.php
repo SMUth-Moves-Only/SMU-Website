9856615371d29d1d1c6902afb1adfa79f8c8168f
Walmufti <79267405+Walmufti@users.noreply.github.com>
2021-03-24 22:11:50 -0400

    update 10:11pm

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include "header.php";
 
 <!--Login buttons for instructor, student, or administrator-->
 
-
 <!DOCTYPE html>
 <html>
 
@@ -12,18 +11,16 @@ include "header.php";
     <link rel="stylesheet" href="css/index.css">
     <script src="js/index.js"></script>
 </head>
+  <body>
 
-<body>
+    <div class="Login">
+      <h1>Log In</h1> <br>
+        <div class="buttons">
+          <button>INSTRUCTOR</button> <br>
+          <button onclick="window.location='studentlogin.php';">STUDENT</button> <br>
+          <button>ADMINISTRATOR</button>
+        </div>
+    </div>
 
-  <div class="Login">
-    <h1>Log In</h1> <br>
-    <div class="buttons">
-              <button>INSTRUCTOR</button> <br>
-              <button onclick="window.location='studentlogin.php';">STUDENT</button> <br>
-              <button>ADMINISTRATOR</button>
-            </div>
-  </div>
-
-</body>
-
+  </body>
 </html>
