823f4cd0903a1a9d024b7ca3957ad678f901d56c
Walmufti <79267405+Walmufti@users.noreply.github.com>
2021-03-24 23:10:14 -0400

    update 11:10pm

--- coursedetails.php
+++ coursedetails.php
@@ -19,26 +19,35 @@ include "header.php";
   <div class="courseInfo">
     <h1>Course Details</h1>
 
-    <p><label>Course Title
-    <input type="text" name="CourseTitle">
-    </label></p>
+    <div class="CT">
+      <p><label>Course Title
+      <input type="text" name="CourseTitle">
+      </label></p>
+    </div>
 
-    <p><label>Course ID
-    <input type="text" name="CourseID">
-    </label></p>
+    <div class="CID">
+      <p><label>Course ID
+      <input type="text" name="CourseID">
+      </label></p>
+    </div>
 
-    <p><label>Instructor
-    <input type="text" name="Instructor">
-    </label></p>
+    <div class="I">
+      <p><label>Instructor
+      <input type="text" name="Instructor">
+      </label></p>
+    </div>
 
-    <p><label>Term
-    <input type="text" name="Term">
-    </label></p>
+    <div class="T">
+      <p><label>Term
+      <input type="text" name="Term">
+      </label></p>
+    </div>
 
-    <p><label>Course Number
-    <input type="text" name="Course Number">
-    </label></p>
-
+    <div class="CN">
+      <p><label>Course Number
+      <input type="text" name="Course Number">
+      </label></p>
+    </div>
 
   </div>
 
