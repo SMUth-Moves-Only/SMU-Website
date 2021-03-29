c933a04670fe08e28c766e7c12aadfb80c9a146c
Greg Fairbanks <gregory12006@yahoo.com>
2021-03-29 19:00:24 -0400

    Update professor_course_import.inc.php

--- includes/professor_course_import.inc.php
+++ includes/professor_course_import.inc.php
@@ -12,27 +12,9 @@ if (isset($_POST['course-import'])) {
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    // Check if image file is a actual image or fake image
-    if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if ($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-
     // Check if file already exists
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }
 
@@ -54,6 +36,7 @@ if (isset($_POST['course-import'])) {
             $courseID = "";
             $termID = "";
             foreach ($csv as &$course) {
+                
                 $sql = "SELECT id FROM course WHERE course_number = ?";
                 $stmt = mysqli_stmt_init($conn);
 
