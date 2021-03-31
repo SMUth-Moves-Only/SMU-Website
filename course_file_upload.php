507af192e215fe40e5677cc917386319c7e29a27
Walmufti <79267405+Walmufti@users.noreply.github.com>
2021-03-31 17:18:07 -0400

    update 5:18

--- course_file_upload.php
+++ course_file_upload.php
@@ -2,14 +2,41 @@
 include "header.php";
 ?>
 
-
-
 <!DOCTYPE html>
 <html>
-
     <link rel="stylesheet" href="css/course_file_upload.css">
-
 <body>
+
+  <h1>Import Students</h1>
+  <textarea name="textbox" rows="7" cols="30"></textarea> <br>
+<br>
+
+<!--select course drop down-->
+<form action="includes/professor_student_add.inc.php" method="post" enctype="multipart/form-data">
+    <select name="CourseSelect">
+        <?php
+        session_start();
+        if (isset($_SESSION['course_info'])) {
+            for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
+                echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
+            }
+        } else {
+            echo '<option value="" disabled selected hidden>No Courses Available</option>';
+        }
+        ?>
+    </select><br>
+</form>
+
+<br>
+
+<!--file directions-->
+  <div class="fileinfo">
+    <p>Student file should be in .csv format.</p>
+    <p>File should have rows in order: id, first_name, <br>
+    last_name, major, email_address, <br>
+    student_password</p>
+  </div>
+<br>
 
 <!--    <form action="includes/professor_course_add.inc.php" method="post" enctype="multipart/form-data">
         Select image to upload:
@@ -18,7 +45,7 @@ include "header.php";
     </form>
 -->
 
-    <form action="includes/professor_student_add.inc.php" method="post" enctype="multipart/form-data">
+  <!--  <form action="includes/professor_student_add.inc.php" method="post" enctype="multipart/form-data">
         <label>Course Title </label>
         <select name="CourseSelect">
             <?php
@@ -32,18 +59,24 @@ include "header.php";
             }
             ?>
         </select><br>
-        <input type="file" name="fileToUpload" id="fileToUpload">
+        </form>
+  -->
+
+  <!--select file button-->
+      <div class="choosefile">
+        <label>Select Student File</label>
+        <input type="file" name="fileToUpload" id="fileToUpload"> <br><br>
+      </div>
+
+    <!--import studnets button-->
+      <button type="submit" value="Import Student" name="Import Students">Import Students</button>
+
+<!--
         <input type="submit" value="Import Student" name="student-import">
-    </form>
-
-
-
+-->
 
 </body>
-
 </html>
-
-
 <?php
 include "footer.php";
 ?>
