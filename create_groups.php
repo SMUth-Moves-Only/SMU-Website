0dfb8e63ce68fb0ad98cca4defff1b7fad2c802e
Walmufti <79267405+Walmufti@users.noreply.github.com>
2021-03-31 18:04:43 -0400

    update 6:04

--- create_groups.php
+++ create_groups.php
@@ -6,26 +6,26 @@ if (!isset($_SESSION['professor_id'])) {
 }
 ?>
 
+<h1>Create Students Groups</h1>
 
-
-<form action="includes/create_group.inc.php" method="post">
-    <label>Course Title </label>
-    <select name="CourseSelect">
-        <?php
-        if (isset($_SESSION['course_info'])) {
-            for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
-                echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
-            }
-        } else {
-            echo '<option value="" disabled selected hidden>No Courses Available</option>';
-        }
-        ?>
-    </select><br>
-    <input type="text" id="teamName" name="teamName" placeholder="teamName">
-    <button type="submit" name="create-group"> Submit </button>
-</form>
-
-
+<div class="cgroup">
+  <form action="includes/create_group.inc.php" method="post">
+      <label>Course Title </label>
+      <select name="CourseSelect">
+          <?php
+          if (isset($_SESSION['course_info'])) {
+              for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
+                  echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
+              }
+          } else {
+              echo '<option value="" disabled selected hidden>No Courses Available</option>';
+          }
+          ?>
+      </select><br>
+      <input type="text" id="teamName" name="teamName" placeholder="teamName">
+      <button type="submit" name="create-group"> Submit </button>
+  </form>
+</div>
 
 <?php
 include "footer.php";
