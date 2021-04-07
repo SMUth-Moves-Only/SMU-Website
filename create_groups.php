7343d57868d6fb2dc43153f8bceff925456e2189
Walmufti <79267405+Walmufti@users.noreply.github.com>
2021-04-07 15:54:53 -0400

    update

--- create_groups.php
+++ create_groups.php
@@ -1,35 +1,64 @@
 <?php
-include "header.php";
-if (!isset($_SESSION['professor_id'])) {
-    header("Location: ./?error=notloggedin");
-    exit();
-}
+  include "header.php";
+  if (!isset($_SESSION['professor_id'])) {
+      header("Location: ./?error=notloggedin");
+      exit();
+  }
 ?>
 
 <!--call style sheet-->
 <link rel="stylesheet" href="css/create_groups.css">
 
-<h1>Create Students Groups</h1>
+<h1>Assign Students </h1>
 
-<!--course title drop down menu-->
+<!--Create Groups-->
 <div class="cgroup">
+  <h2>Create Groups</h2>
   <form action="includes/create_group.inc.php" method="post">
-      <label>Course Title </label>
-      <select name="CourseSelect">
-          <?php
-          if (isset($_SESSION['course_info'])) {
-              for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
-                  echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
-              }
+    <label>Course Name:</label>
+    <select name="CourseSelect">
+      <?php
+        if (isset($_SESSION['course_info'])) {
+          for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
+              echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
+          }
           } else {
               echo '<option value="" disabled selected hidden>No Courses Available</option>';
           }
-          ?>
-      </select><br>
+      ?>
+    </select>
 
-      <!--team name & submit button-->
-      <input type="text" id="teamName" name="teamName" placeholder="teamName">
-      <button type="submit" name="create-group"> Submit </button>
+    <!--team name & submit button-->
+    <input type="text" id="teamName" name="teamName" placeholder="Group Name"><br>
+    <button type="submit" name="create-group"> Submit </button>
+  </form>
+</div>
+
+<div class="cgroup">
+  <h2></h2>
+  <form action="includes/create_group.inc.php" method="post">
+    <label>Course Name:</label>
+    <select name="CourseSelect">
+      <?php
+        if (isset($_SESSION['course_info'])) {
+          for ($i = 0; $i < count($_SESSION['course_info']); $i++) {
+              echo '<option value="' . $_SESSION['course_info'][$i][0] . '">' . $_SESSION['course_info'][$i][2] . '</option>';
+          }
+          } else {
+              echo '<option value="" disabled selected hidden>No Courses Available</option>';
+          }
+      ?>
+    </select><br>
+
+    <label>Groups:</label>
+    <select name="groupselect">
+    </select><br>
+    <label>Students In Group</label>
+    <select name="stuInGroup">
+    </select>
+    <br>
+    <button type="submit" name="create-group"> Submit </button>
+
   </form>
 </div>
 
